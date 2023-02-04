c00bf32d1628054f3344f2322f684716302e7bc4
2004240023 <2004240023yr@edu.oca.ac.jp>
2023-02-04 14:01:19 +0900

    comit

---
+++ try_typescript/src/ex1.ts
@@ -0,0 +1,21 @@
+// const isOpen : boolean = true;
+// console.log(isOpen);
+// const disabled : boolean = false;
+// console.log(disabled);
+// const list1: number[] = [1, 2, 3];
+// console.log(list1);
+// const list2: Array<number> = [1, 2, 3];
+// console.log(list2);
+// const noValue1 : undefined = undefined;
+// console.log(noValue1);
+// const noValue2 : null = null;
+// console.log(noValue2);
+
+// function sayHello1(name:string):string{return `Hello ${name}!`}
+// const sayHello2 : (name: string)=> string = (name) => { return `Hello ${name}!`}
+
+//演習
+const Return: ()=> void = (): void =>{
+    console.log('Hello, TypeScript');
+}
+Return();
---
+++ try_typescript/src/index.ts
@@ -0,0 +1,8 @@
+const message: string = "Hello, TypeScript";
+//const message: number = "Hello, TypeScript";
+// src/index.ts:2:7 - error TS2322: Type 'string' is not assignable to type 'number'.
+// 2 const message: number = "Hello, TypeScript";
+//         ~~~~~~~
+// Found 1 error in src/index.ts:2
+
+console.log(message);
